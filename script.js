53a19829fcbb18da8c86af36c19a1068ba7fbe65
Gevorg Davtyan <gevorgdavtyan99@mail.ru>
2020-06-26 15:10:13 +0400

    i changed this file

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-let slideBtn=document.querySelector('.slides__button')
-let slides=document.querySelectorAll('.slides .slide')
-let figure=document.querySelector('.triangle')
-let i=0
-let j=2
-figure.classList.add('gotei1')
-let item
-	slideBtn.onclick=function(){
-		item=figure.classList.item(1)
-		slides[i].classList.remove('op')
-		i++
-		if(i==3){
-			i=0
-		}
-		slides[i].classList.add('op')
-		
-		figure.classList.remove(item)
-		go('gotei'+j)
-		j++
-		if(j==4){
-			j=1
-		}
-	}
+let slideBtn = document.querySelector(".slides__button");
+let slides = document.querySelectorAll(".slides .slide");
+let figure = document.querySelector(".triangle");
+let i = 0;
+let j = 2;
+figure.classList.add("gotei1");
+let item;
+slideBtn.onclick = function () {
+  item = figure.classList.item(1);
+  slides[i].classList.remove("op");
+  i++;
+  if (i == 3) {
+    i = 0;
+  }
+  slides[i].classList.add("op");
 
-function go(elem){
-	figure.classList.add(elem)
+  figure.classList.remove(item);
+  go("gotei" + j);
+  j++;
+  if (j == 4) {
+    j = 1;
+  }
+};
+
+function go(elem) {
+  figure.classList.add(elem);
+  console.log(1);
 }
